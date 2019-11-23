d70e95ae2f7e65b835cbdbcd3562d4624c1ac597
tett23 <tett23@gmail.com>
2019-11-24 01:32:52 +0900

    Add stopped transformer

---
+++ src/transformers/aspect/stopped.rs
@@ -0,0 +1,45 @@
+use super::super::Transformer;
+
+pub struct Stopped {
+  buffer: String,
+}
+
+impl Stopped {
+  pub fn new(buffer: String) -> Self {
+    Stopped { buffer }
+  }
+}
+
+impl Transformer for Stopped {
+  fn is_stopped(&self) -> bool {
+    true
+  }
+
+  fn push(&mut self, _: char) -> Box<dyn Transformer> {
+    Box::new(Stopped::new(self.buffer_content()))
+  }
+
+  fn cancel(&mut self) -> Box<dyn Transformer> {
+    Box::new(Stopped::new("".to_string()))
+  }
+
+  fn enter(&mut self) -> Box<dyn Transformer> {
+    Box::new(Stopped::new(self.buffer_content()))
+  }
+
+  fn tab(&mut self) -> Box<dyn Transformer> {
+    Box::new(Stopped::new(self.buffer_content()))
+  }
+
+  fn space(&mut self) -> Box<dyn Transformer> {
+    Box::new(Stopped::new(self.buffer_content()))
+  }
+
+  fn buffer_content(&self) -> String {
+    self.buffer.clone()
+  }
+
+  fn display_string(&self) -> String {
+    self.buffer.clone()
+  }
+}
